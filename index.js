f001dfdf7f3c9bb4b17e593633e39b5db600c67c
Brian Thompson <bthompson50@mail.valenciacollege.edu>
2021-09-18 10:58:05 -0700

    Completed JavaScript Initial Assessment assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+//Declare Variables
+let firstName;
+let lastName;
+let age;
+
+firstName = "Brian";
+lastName = "Thompson";
+age = "42";
+
+alert("My name is: " + firstName + " " + lastName);
+alert("My age is: " + age);
+console.log("This class has been lots of fun so far!");
